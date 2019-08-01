b150158658481b448aae4b195f32502167d31e3a
Alexander Rice <webdev@ricealexander.com>
2019-08-01 15:47:46 -0500

    :building_construction: Group related helpers in their own files
    Multiple exports should be themed around a topic. Differentiate grouped files with a plural form in the file name

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { formatTime as sampleFunction } from './lib';
+import { formatTimestamp as sampleFunction } from './lib';
 
 const samples = [90, '12345', '10', '10000000000', 'abc'];
 console.log(samples.map(sample => sampleFunction(sample)));
