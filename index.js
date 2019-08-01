fed79448d7779e424b53be17343f0030ae3f3a77
Alexander Rice <webdev@ricealexander.com>
2019-08-01 17:46:09 -0500

    :sparkles: Create helper parseKeyTrail

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-import { round } from './lib';
+import { parseKeyTrail } from './lib';
 
-const pi = 3.14159265359;
-console.log([
-  round(pi, 2),
-  round(pi, '2'),
-  round(pi),
-  round('abc'),
-]);
+const episode = {
+  audio: 'path/to/audio',
+  chapter: {
+    number: 1,
+    episode: 1,
+    title: 'Chapter Title Here',
+  },
+  speaker: {
+    name: 'Jane Doe',
+    title: 'Science and Engineering award-winner',
+    photos: {
+      '5000x3333': 'jane-doe_5000x3333.jpg',
+      '1080x700': 'jane-doe_1080x700.jpg',
+    },
+  },
+};
+
+console.log({
+  audio: parseKeyTrail(episode, 'audio'),
+  chapterTitle: parseKeyTrail(episode, 'chapter.title'),
+  photo: parseKeyTrail(episode, 'speaker.photos.1080x700'),
+  notExists: parseKeyTrail(episode, 'speaker.photos.300x200'),
+});
