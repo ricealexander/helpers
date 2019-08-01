57186fabe806d36e10c593b084e75cffaa8af808
Alexander Rice <webdev@ricealexander.com>
2019-08-01 17:09:40 -0500

    :sparkles: Create helper round

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-import { handleBounds } from './lib';
+import { round } from './lib';
 
-const limit = 10;
-const dynamicValue = 5;
-
-console.log({
-  smaller: handleBounds(-1, limit),
-  larger: handleBounds(15, limit),
-  insideRange: handleBounds(dynamicValue, limit),
-  addition: handleBounds(dynamicValue + 6, limit),
-  subtraction: handleBounds(dynamicValue - 6, limit),
-});
+const pi = 3.14159265359;
+console.log([
+  round(pi, 2),
+  round(pi, '2'),
+  round(pi),
+  round('abc'),
+]);
