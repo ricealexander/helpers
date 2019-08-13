1333558a0bbb4699d201d35657ab97f7d71f3066
ricealexander <webdev@ricealexander.com>
2019-08-12 20:22:20 -0500

    :construction: Begin working on color converter

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-import { parseKeyTrail } from './lib';
-
-const episode = {
-  audio: 'path/to/audio',
-  chapter: {
-    number: 1,
-    episode: 1,
-    title: 'Chapter Title Here',
-  },
-  speaker: {
-    name: 'Jane Doe',
-    title: 'Science and Engineering award-winner',
-    photos: {
-      '5000x3333': 'jane-doe_5000x3333.jpg',
-      '1080x700': 'jane-doe_1080x700.jpg',
-    },
-  },
-};
+import { getHexColor } from './lib/helpers/color-converters';
 
 console.log({
-  audio: parseKeyTrail(episode, 'audio'),
-  chapterTitle: parseKeyTrail(episode, 'chapter.title'),
-  photo: parseKeyTrail(episode, 'speaker.photos.1080x700'),
-  notExists: parseKeyTrail(episode, 'speaker.photos.300x200'),
+  twoDigit: getHexColor('#12'),
+  threeDigit: getHexColor('#123'),
+  sixDigit: getHexColor('#123456'),
+  eightDigit: getHexColor('#12345600'),
 });
