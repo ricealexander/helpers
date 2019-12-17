25e27e50268a20888b65de9f9fcf1fa77048a139
Alexander Rice <webdev@ricealexander.com>
2019-12-17 11:27:08 -0600

    :recycle: Flip simplifyString's chain
    lowercasing before stripping extraneous characters makes for a simpler RegExp

--- helpers/simplifyString.js
+++ helpers/simplifyString.js
@@ -1,12 +1,14 @@
 const simplifyString = string => {
   if (typeof string !== 'string') {
-    throw new TypeError(`Argument must be of type string. Instead got ${typeof string}`)
+    throw new TypeError(
+      `Argument must be of type string. Instead got ${typeof string}`
+    )
   }
   // \W matches all characters except A-Za-z0-9_
   // we want to also exclude the _ character
   return string
-    .replace(/[\W_]/g, '')
     .toLowerCase()
+    .replace(/[^a-z]+/g, '')
 }
 
 export default simplifyString
