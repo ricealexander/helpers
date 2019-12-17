35565e64869fec76f543fea9c9ce2ba364c9681d
Alexander Rice <webdev@ricealexander.com>
2019-12-17 11:34:37 -0600

    :sparkles: Create getExtensionFromFileName

---
+++ helpers/getExtensionFromFileName.js
@@ -0,0 +1,15 @@
+function getExtensionFromFileName(file) {
+  if (typeof file !== 'string') {
+    throw new TypeError(
+      `Argument must be of type "string". Instead got "${typeof file}"`,
+    )
+  }
+
+  const match = file.match(/^(.*)(\.[\dA-Za-z]+)$/)
+  if (!match) return null
+
+  const [_file, _filename, extension] = match
+  return extension.toLowerCase()
+}
+
+export default getExtensionFromFileName
