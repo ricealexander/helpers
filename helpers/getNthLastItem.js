c3ef6a02fe24de08301423ad27afc68b8fb67aea
Alexander Rice <webdev@ricealexander.com>
2019-12-17 11:39:48 -0600

    :reycle: Build stronger validation for getNthLastItem

--- helpers/getNthLastItem.js
+++ helpers/getNthLastItem.js
@@ -1,7 +1,18 @@
 const getNthLastItem = (array, n = 1) => {
-  if (typeof n !== 'number') throw new TypeError(`Expected number, instead got ${typeof n}`)
-  if (n > array.length) throw new RangeError('Index cannot be larger than array')
-  if (n < 0) throw new RangeError('Index must have a positive index')
+  if (!Array.isArray(array)) {
+    throw new TypeError('First Argument must be an array')
+  }
+  if (typeof n !== 'number') {
+    throw new TypeError(
+      `Second Argument expected anumber, instead got ${typeof n}`
+    )
+  }
+  if (n < 0) {
+    throw new RangeError('Second Argument must be a positive number')
+  }
+  if (n > array.length) {
+    throw new RangeError('Index must not be larger than the array')
+  }
 
   return array[array.length - n]
 }
