1aab5639216c50439658550be95b0c1bc3f7fc4f
Alexander Rice <webdev@ricealexander.com>
2019-12-17 13:03:11 -0600

    :recycle: Extract look-up tables to a higher scope
    This optimization :one: possibly optimizes for multiple function calls. :two: makes all of the function logic visible at the same time.

--- helpers/getPreloadType.js
+++ helpers/getPreloadType.js
@@ -1,3 +1,38 @@
+const preloadTypes = {
+  // Audio Files
+  mp3:   'audio',
+  ogg:   'audio',
+  wav:   'audio',
+
+  // Image Files
+  jpg:   'image',
+  jpeg:  'image',
+  gif:   'image',
+  png:   'image',
+  svg:   'image',
+  webp:  'image', // developed by Google
+  avif:  'image', // developed by Apple
+  heic:  'image', // developed by Apple
+  heif:  'image', // developed by Alliance for Open Media
+
+  // Font Formats
+  eot:   'font',
+  otf:   'font',
+  ttf:   'font',
+  woff:  'font',
+  woff2: 'font',
+
+  // Data Files
+  csv:   'fetch',
+  json:  'fetch',
+  txt:   'fetch',
+  xml:   'fetch',
+
+  // Other
+  js:    'script',
+  css:   'style',
+}
+
 function getPreloadType (_extension) {
   if (typeof _extension !== 'string') {
     throw new TypeError(
@@ -8,41 +43,7 @@ function getPreloadType (_extension) {
   let extension = _extension.toLowerCase()
   if (extension.startsWith('.')) extension = extension.slice(1)
 
-  const formats = {
-    // Audio Files
-    mp3:   'audio',
-    ogg:   'audio',
-    wav:   'audio',
-
-    // Image Files
-    jpg:   'image',
-    jpeg:  'image',
-    gif:   'image',
-    png:   'image',
-    svg:   'image',
-    webp:  'image', // developed by Google
-    avif:  'image', // developed by Apple
-    heic:  'image', // developed by Apple
-    heif:  'image', // developed by Alliance for Open Media
-
-    // Font Formats
-    eot:   'font',
-    otf:   'font',
-    ttf:   'font',
-    woff:  'font',
-    woff2: 'font',
-
-    // Data Files
-    csv:   'fetch',
-    json:  'fetch',
-    txt:   'fetch',
-    xml:   'fetch',
-
-    // Other
-    js:    'script',
-    css:   'style',
-  }
-  return formats[extension] || null
+  return preloadTypes[extension] || null
 }
 
 export default getPreloadType
