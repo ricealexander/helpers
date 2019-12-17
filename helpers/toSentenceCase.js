379d914b5b8318744ebfe828e6153f1c394ea3e7
Alexander Rice <webdev@ricealexander.com>
2019-12-17 12:04:15 -0600

    :sparkles: Create toSentenceCase()

---
+++ helpers/toSentenceCase.js
@@ -0,0 +1,8 @@
+const toSentenceCase = string => (
+  string
+    .split(/[-_]/)
+    .map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ')
+)
+
+export default toSentenceCase
