9ea3a7e237476b9388fbd3ce94b2460bb92ef186
Alexander Rice <webdev@ricealexander.com>
2020-01-24 14:22:43 -0600

    :bulb: Document parseKeyTrail helper
    parseKeyTrail accepts an object and a string representation of the path to an object property. It was built to solve the same problem as optional chaining, that in object.property.longProperty.evenLongerProperty any property can be undefined, which causes their nested properties to throw an error.
    :thinking: parseKeyTrail(object, 'property.longProperty.evenLongerProperty') would handle this solution by returning the value of the deepest nested property or 'undefined' if it couldn't.
    :bulb: document this behavior and mention optional chaining. Optional chaining may result in deprecating this helper or it may power up the function further, allowing us to remove the more confusing 'followTrail' helper.
    :thinking: The only use case I can imagine for not deprecating this function is for abstractions, such as the data-helpers

--- helpers/parseKeyTrail.js
+++ helpers/parseKeyTrail.js
@@ -3,14 +3,30 @@
 // and an object and returns the value of the object at the key trail
 // returns undefined if key doesn't exist
 
-// demo:
-// var object = {a: { b: { c: 'd'}}};
-// console.log(parseKeyTrail(object, 'a.b.c')); // returns: 'd'
+/*
+const object = {prop: { longProp: { evenLongerProp: 'value'}}};
 
-function parseKeyTrail (object, string) {
+// without parseKeyTrail
+// because any object property could be set to undefined
+// we need to check each one in order to prevent throwing an error
+
+const value =
+  object.prop &&
+  object.prop.longProp &&
+  object.prop.longProp.evenLongerProp
+
+// with parseKeyTrail
+const value = parseKeyTrail(object, 'prop.longProp.evenLongerProp')
+
+// this need may be eliminated by optional chaining
+const value = object.prop?.longProp?.evenLongerProp
+
+*/
+
+function parseKeyTrail (object, trail) {
   if (
-    !string || typeof string !== 'string' ||
-    !object || typeof object !== 'object'
+    !object || typeof object !== 'object' ||
+    !trail || typeof trail !== 'string'
   ) return undefined
 
   function followTrail (objectLevel, key) {
@@ -18,8 +34,19 @@ function parseKeyTrail (object, string) {
     return objectLevel[key]
   }
 
-  const trail = string.split('.')
-  return trail.reduce((result, value) => followTrail(result, value), object)
+  return trail
+    .split('.')
+    .reduce((result, value) => followTrail(result, value), object)
 }
+
+// function parseKeyTrail (object, trail) {
+//   if (!object || typeof object !== 'object' ||
+//       !trail  || typeof trail !== 'string'
+//   ) return undefined
+
+//   return trail
+//     .split('.')
+//     .reduce((level, value) => level?[value], object)
+// }
 
 export default parseKeyTrail
