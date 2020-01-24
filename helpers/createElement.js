e7a0a8b42d143d49cd03546701caa65f07edac05
Alexander Rice <webdev@ricealexander.com>
2020-01-24 13:38:33 -0600

    :recycle: Power up createElement helper
    createElement accepted a selector-inspired shorthand and an optional innerHTML argument.
    :bulb: To make this function more versatile, remove the innerHTML parameter and replace it with a properties object. This essentially creates two formats.
    :one: Emmet-like shorthand is still acceptable and is fully supported. Properties object can be used solely for cases when it's necessary, such as setting .innerHTML
    :two: selector argument can be a string representation of a tag name 'div', such as is the case for document.createElement(). The properties argument can then be used to set all of the proprties for the element in one step.
    :three: These formats can be mixed as needed. When there is a conflict between the shorthand and properties object, the properties object overwrites the shorthand. This is purposeful because the properties object may be dynamically generated.
    :sparkles: The properties object determines whether a property is a standard property of the Element object. If the property is not, we assume that the property is an attribute

--- helpers/createElement.js
+++ helpers/createElement.js
@@ -1,10 +1,45 @@
+/* eslint-disable max-len */
 /* eslint-disable no-param-reassign */
 
 // createElement
-// creates an element based on an emmet-like shorthand
-// tag.class#id[attribute=value]{text content}
+// a utility to simplify creating an element and adding properties to it.
+// it offers two formats, which can be combined,
+//   1. An selector-based shorthand: tag.class#id[attribute=value]{text content}
+//   2. A properties object
 
-function createElement (_selector, innerHTML = '') {
+// properties object is useful for properties such as .innerHTML,
+// which are not supported with the selector shorthand,
+// and for use cases where an element's property is dynamic.
+
+/*
+  // without createElement
+  const button = document.createElement('button')
+  button.id = "subscribe"
+  button.className = "btn btn-danger"
+  button.setAttribute("aria-describedby", "why-to-subscribe")
+  button.innerText = "Subscribe"
+
+  // using selector-based shorthand
+  const button = createElement(
+    'button#subscribe.btn.btn-danger[aria-describedby="why-to-subscribe"]{Subscribe}'
+  )
+
+  // using properties object
+  const button = createElement('button', {
+    id: "subscribe",
+    className: "btn btn-danger",
+    "aria-describedby": "why-to-subscribe",
+    innerText: "Subscribe"
+  })
+
+  // these formats can be mixed together
+  const button = createElement('button.btn.btn-danger{Subscribe}', {
+    id: "subscribe",
+    "aria-describedby": "why-to-subscribe"
+  })
+*/
+
+function createElement (_selector, properties = {}) {
   if (typeof _selector !== 'string') {
     throw new TypeError(`Argument must be a string. Instead got ${typeof _selector}.`)
   }
@@ -16,47 +51,56 @@ function createElement (_selector, innerHTML = '') {
     throw new Error('Argument must start with a letter.')
   }
 
-  const segmentPattern = /(^[A-Za-z]|[#.[{])[^#.[{]+/g // delimeting characters are . # [
+  const segmentPattern = /(^[A-Za-z]|[#.[{])[^#.[{]+/g // delimeting characters are . # [ {
   const [tagName, ...attributes] = selector.match(segmentPattern)
 
-  const longhand = attributes.reduce(
-    (element, attribute) => {
-      // handle class
-      if (attribute.startsWith('.')) element.classList.add(attribute.slice(1))
+  // create the element!
+  const element = document.createElement(tagName)
 
-      // handle id
-      if (attribute.startsWith('#')) element.id = attribute.slice(1)
+  // add properties from the selector
+  attributes.forEach(attribute => {
+    // handle class
+    if (attribute.startsWith('.')) element.classList.add(attribute.slice(1))
 
-      // handle attribute
-      if (attribute.startsWith('[')) {
-        const attributeValuePattern = /\[([^=]+)="?([^"\]]+)"?]/
-        const attributePattern = /\[([^=]+)]/
+    // handle id
+    if (attribute.startsWith('#')) element.id = attribute.slice(1)
 
-        // matches [attribute="value"] or [attribute=value]
-        if (attributeValuePattern.test(attribute)) {
-          // eslint-disable-next-line max-len
-          const [_, attributeName, value] = attribute.match(attributeValuePattern)
-          element.setAttribute(attributeName, value)
-        }
+    // handle attribute
+    if (attribute.startsWith('[')) {
+      const attributeValuePattern = /\[([^=]+)="?([^"\]]+)"?]/
+      const attributePattern = /\[([^=]+)]/
 
-        if (attributePattern.test(attribute)) { // matches [attribute]
-          const [_, attributeName] = attribute.match(attributePattern)
-          element.setAttribute(attributeName, '')
-        }
+      // matches [attribute="value"] or [attribute=value]
+      if (attributeValuePattern.test(attribute)) {
+        // eslint-disable-next-line max-len
+        const [_, attributeName, value] = attribute.match(attributeValuePattern)
+        element.setAttribute(attributeName, value)
       }
 
-      // handle textContent
-      if (attribute.startsWith('{')) {
-        const [_, textContent] = attribute.match(/{([^}]+)}/) // matches {any content here}
-        element.textContent += textContent
+      if (attributePattern.test(attribute)) { // matches [attribute]
+        const [_, attributeName] = attribute.match(attributePattern)
+        element.setAttribute(attributeName, '')
       }
-      return element
-    },
-    document.createElement(tagName)
-  )
+    }
 
-  longhand.insertAdjacentHTML('beforeend', innerHTML)
-  return longhand
+    // handle textContent
+    if (attribute.startsWith('{')) {
+      const [_, textContent] = attribute.match(/{([^}]+)}/) // matches {any content here}
+      element.textContent += textContent
+    }
+  })
+
+  // add properties from the properties object
+  Object.entries(properties).forEach(([property, value]) => {
+    // if the property is not recognized by element,
+    // we must treat it as an attribute
+    const isNativeProperty = (property in element)
+
+    if (isNativeProperty) element[property] = value
+    else element.setAttribute(property, value)
+  })
+
+  return element
 }
 
 export default createElement
